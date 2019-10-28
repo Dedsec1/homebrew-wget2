27ec6b50fac3393a557bf7165e60e7173202bdb9
Dedsec1 <Dedsec1@users.noreply.github.com>
2019-10-27 22:13:50 -0400

    Update wget2.rb

--- Formula/wget2.rb
+++ Formula/wget2.rb
@@ -1,7 +1,7 @@
 class Wget2 < Formula
   desc "Multithreaded metalink/file/website downloader"
   homepage "https://gitlab.com/gnuwget/wget2/"
-  url "https://alpha.gnu.org/gnu/wget/wget2-1.99.1.tar.gz"
+  url "https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz"
   sha256 "8d9b0b0897ff9623169db47c656d09007677f8d51de5f5cd3652258aab8b4dde"
 
   depends_on "gettext"
